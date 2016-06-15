49539701f16e219c4945676815a49c24456d8373
hbrls <shuaizhexu@gmail.com>
2016-06-15 17:28:49 +0800

    remove sentry

--- analytics.js
+++ analytics.js
@@ -7134,8 +7134,7 @@ module.exports = {
   'route': require('analytics.js-integration-route'),
   'saasquatch': require('analytics.js-integration-saasquatch'),
   'satismeter': require('analytics.js-integration-satismeter'),
-  'segmentio': require('analytics.js-integration-segmentio'),
-  'sentry': require('analytics.js-integration-sentry')
+  'segmentio': require('analytics.js-integration-segmentio')
 };
 
 }, {
@@ -7209,8 +7208,7 @@ module.exports = {
   "analytics.js-integration-route":146,
   "analytics.js-integration-saasquatch":147,
   "analytics.js-integration-satismeter":148,
-  "analytics.js-integration-segmentio":149,
-  "analytics.js-integration-sentry":150}],
+  "analytics.js-integration-segmentio":149}],
 79: [function(require, module, exports) {
 
 /**
@@ -18786,63 +18784,6 @@ function utm(query){
 }
 
 }, {"querystring":27}],
-150: [function(require, module, exports) {
-
-/**
- * Module dependencies.
- */
-
-var integration = require('analytics.js-integration');
-var is = require('is');
-
-/**
- * Expose `Sentry` integration.
- */
-
-var Sentry = module.exports = integration('Sentry')
-  .global('Raven')
-  .global('RavenConfig')
-  .option('config', '')
-  .tag('<script src="//cdn.ravenjs.com/1.1.16/native/raven.min.js">');
-
-/**
- * Initialize.
- *
- * http://raven-js.readthedocs.org/en/latest/config/index.html
- * https://github.com/getsentry/raven-js/blob/1.1.16/src/raven.js#L734-L741
- *
- * @api public
- */
-
-Sentry.prototype.initialize = function() {
-  var dsn = this.options.config;
-  window.RavenConfig = { dsn: dsn };
-  this.load(this.ready);
-};
-
-/**
- * Loaded?
- *
- * @api private
- * @return {boolean}
- */
-
-Sentry.prototype.loaded = function() {
-  return is.object(window.Raven);
-};
-
-/**
- * Identify.
- *
- * @api public
- * @param {Identify} identify
- */
-
-Sentry.prototype.identify = function(identify) {
-  window.Raven.setUser(identify.traits());
-};
-
-}, {"analytics.js-integration":166,"is":18}],
 219: [function(require, module, exports) {
 
 /**
